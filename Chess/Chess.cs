88d7b5e8cda0f270af19b4996989d1aafaff95bb
ebaysher <tn_kz@mail.ru>
2021-05-05 11:38:22 +0600

    ..

--- Chess/Chess.cs
+++ Chess/Chess.cs
@@ -2,7 +2,18 @@
 
 namespace Chess
 {
-    public class Class1
+    public class Chess
     {
+        public string fen { get; private set; }
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.fen = fen;
+        }
+
+        public Chess Move(string move)
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess;
+        }
     }
 }
